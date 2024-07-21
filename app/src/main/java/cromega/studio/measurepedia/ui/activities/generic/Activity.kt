5522412c78a835aa6e559d3c900c08558d4eebb5
Cromega08 <cr.jrg08@gmail.com>
2024-07-21 14:58:39 -0500

    feat: include "changing language" method on "Activity.kt"

--- app/src/main/java/cromega/studio/measurepedia/ui/activities/generic/Activity.kt
+++ app/src/main/java/cromega/studio/measurepedia/ui/activities/generic/Activity.kt
@@ -1,5 +1,6 @@
 package cromega.studio.measurepedia.ui.activities.generic
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,7 @@ import cromega.studio.measurepedia.data.database.tables.instances.PersonsTable
 import cromega.studio.measurepedia.data.database.tables.instances.RecordsTable
 import cromega.studio.measurepedia.resources.utils.ResourcesUtils
 import cromega.studio.measurepedia.resources.utils.TablesUtils
+import java.util.Locale
 
 abstract class Activity: ComponentActivity()
 {
@@ -26,6 +28,18 @@ abstract class Activity: ComponentActivity()
                 fieldsTable = FieldsTable(applicationContext),
                 recordsTable = RecordsTable(applicationContext)
             )
+    }
+
+    fun setLocale(language: String)
+    {
+        val locale: Locale = Locale(language)
+        Locale.setDefault(locale)
+
+        val configuration: Configuration = resources.configuration
+
+        configuration.setLocale(locale)
+
+        resources.updateConfiguration(configuration, resources.displayMetrics)
     }
 
     override fun onDestroy()
