c00904db28bec5b653f3d93ae2b05fe49700e3e3
Cromega08 <cr.jrg08@gmail.com>
2024-08-06 21:56:00 -0500

    feat: include the first elements for "MeasuresScreen.Footer"

--- app/src/main/java/cromega/studio/measurepedia/ui/activities/measures/MeasuresScreen.kt
+++ app/src/main/java/cromega/studio/measurepedia/ui/activities/measures/MeasuresScreen.kt
@@ -1,8 +1,24 @@
 package cromega.studio.measurepedia.ui.activities.measures
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import cromega.studio.measurepedia.R
+import cromega.studio.measurepedia.resources.utils.ResourcesUtils
+import cromega.studio.measurepedia.ui.components.elements.RoundedCornerButton
+import cromega.studio.measurepedia.ui.components.elements.SpacerVerticalSmall
+import cromega.studio.measurepedia.ui.components.elements.TextSubtitle
 import cromega.studio.measurepedia.ui.components.elements.TextTitle
 import cromega.studio.measurepedia.ui.components.layouts.GenericBodyLazyColumn
 import cromega.studio.measurepedia.ui.components.layouts.GenericFooterRow
@@ -35,6 +51,20 @@ internal object MeasuresScreen
     @Composable
     fun Footer() =
         GenericFooterRow {
+            RoundedCornerButton(
+                modifier = Modifier.fillMaxWidth(0.8f),
+                onClick = { MeasuresState.openHomeActivity() }
+            ) {
+                Column {
+                    SpacerVerticalSmall()
 
+                    Text(
+                        modifier = Modifier.scale(1.5f),
+                        text = ResourcesUtils.getString(R.string.update_measures)
+                    )
+
+                    SpacerVerticalSmall()
+                }
+            }
         }
 }
